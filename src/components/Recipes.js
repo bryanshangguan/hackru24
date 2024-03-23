78b0612bcdda0f9a3347c5810f673415511584a0
Bryan Shangguan <shangguan.bryan@gmail.com>
2024-03-23 16:57:14 -0400

    tailwind and nextui

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Recipes() {
+function Recipes({ recipes }) {
     return (
         <div className='Recipes-page'>
             <h1>Recipes page</h1>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -15,36 +15,36 @@ function Sidebar({ onMenuSelect }) {
     };
 
     const getButtonClasses = () => {
-        return 'flex items-center text-left py-2 px-4 w-full text-gray-700 hover:bg-blue-hover hover:text-white';
+        return 'flex items-center justify-center text-center py-2 px-4 w-full text-gray-700 text-xl transition-transform duration-150 transform hover:scale-105';
     };
 
     const getSidebarClasses = () => {
-        return isCollapsed ? 'w-20' : 'w-40';
+        return isCollapsed ? 'w-24' : 'w-48';
     };
 
     return (
-        <div className={`fixed top-0 left-0 h-screen ${getSidebarClasses()} bg-blue-accent flex flex-col justify-between p-3 transition-width duration-300`}>
-            <div>
-                <div className={`w-full mb-8 transition-all duration-300 ${isCollapsed ? 'h-16' : 'h-auto'}`}>
-                    <img src={logo1} alt='Logo' className={`w-full h-auto object-contain ${isCollapsed ? 'scale-75' : 'scale-100'}`} />
-                </div>
-                <nav className='space-y-2'>
-                    <button className={getButtonClasses()} onClick={() => handleMenuClick('home')}>
-                        <FontAwesomeIcon icon={faHome} className='text-lg text-center' /> {!isCollapsed && 'Home'}
-                    </button>
-                    <button className={getButtonClasses()} onClick={() => handleMenuClick('pantry')}>
-                        <FontAwesomeIcon icon={faUtensils} className='mr-2 text-lg' /> {!isCollapsed && 'Pantry'}
-                    </button>
-                    <button className={getButtonClasses()} onClick={() => handleMenuClick('recipes')}>
-                        <FontAwesomeIcon icon={faBookOpen} className='mr-2 text-lg' /> {!isCollapsed && 'Recipes'}
-                    </button>
-                    <button className={getButtonClasses()} onClick={() => handleMenuClick('history')}>
-                        <FontAwesomeIcon icon={faHistory} className='mr-2 text-lg' /> {!isCollapsed && 'History'}
-                    </button>
-                </nav>
+        <div className={`fixed top-0 left-0 h-screen ${getSidebarClasses()} bg-blue-accent flex flex-col items-center p-3 transition-width duration-300`}>
+            <div className={`w-full mb-8 transition-all duration-300 ${isCollapsed ? 'h-16' : 'h-auto'}`}>
+                <img src={logo1} alt='Logo' className={`self-center object-contain ${isCollapsed ? 'scale-75' : 'scale-75'}`} />
             </div>
-            <div className='cursor-pointer' onClick={toggleSidebar}>
-                <FontAwesomeIcon icon={isCollapsed ? faArrowRight : faArrowLeft} />
+            <nav className='space-y-2 w-full'>
+                <button className={getButtonClasses()} onClick={() => handleMenuClick('home')}>
+                    <FontAwesomeIcon icon={faHome} className='mr-2 text-xl' /> {!isCollapsed && 'Home'}
+                </button>
+                <button className={getButtonClasses()} onClick={() => handleMenuClick('pantry')}>
+                    <FontAwesomeIcon icon={faUtensils} className='mr-2 text-xl' /> {!isCollapsed && 'Pantry'}
+                </button>
+                <button className={getButtonClasses()} onClick={() => handleMenuClick('recipes')}>
+                    <FontAwesomeIcon icon={faBookOpen} className='mr-2 text-xl' /> {!isCollapsed && 'Recipes'}
+                </button>
+                <button className={getButtonClasses()} onClick={() => handleMenuClick('history')}>
+                    <FontAwesomeIcon icon={faHistory} className='mr-2 text-xl' /> {!isCollapsed && 'History'}
+                </button>
+            </nav>
+            <div className='mt-auto'>
+                <div className='cursor-pointer' onClick={toggleSidebar}>
+                    <FontAwesomeIcon icon={isCollapsed ? faArrowRight : faArrowLeft} className='text-xl' />
+                </div>
             </div>
         </div>
     );
