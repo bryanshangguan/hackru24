d60e867622a2c071cf462717c8b72655f1fd40d5
chrischan04 <cwhc2021@gmail.com>
2024-03-24 09:51:04 -0400

    favorite

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Card, CardBody, Accordion, AccordionItem } from '@nextui-org/react';
+import { Button, Card, CardBody, Accordion, AccordionItem } from '@nextui-org/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
-function Favorites({ favorites }) {
+function Favorites({ toggleFavorite, favorites }) {
     if (favorites == null || favorites.length === 0) {
-        return <p>No favorites added yet.</p>;
+        return <p className='text-3xl font-bold mt-4 px-20'>No favorites added yet.</p>;
     }
 
     return (
@@ -13,11 +15,20 @@ function Favorites({ favorites }) {
                 <Card key={index}>
                     <CardBody>
                         <Accordion>
-                            <AccordionItem title={recipe.name}>
-                                {recipe.steps.map((step, stepIndex) => (
-                                    <p key={stepIndex}>{step}</p>
-                                ))}
-                            </AccordionItem>
+                        <AccordionItem title={
+                            <div className="flex justify-between items-center">
+                                <span>{recipe.name}</span>
+                                    <div className="flex gap-4 items-center">
+                                        <Button onClick={() => toggleFavorite(recipe)} color="white" aria-label="Favorite">
+                                            {favorites.includes(recipe) ? <FontAwesomeIcon icon={faStar} className='text-xl' style={{ color: "#FFD43B", }} /> : <FontAwesomeIcon icon={faStar} className='text-xl' style={{ color: "#000000", }} />}
+                                        </Button>
+                                    </div>
+                                </div>
+                                }>
+                            {recipe.steps.map((step, stepIndex) => (
+                            <p key={stepIndex}>{step}</p>
+                            ))}
+                         </AccordionItem>
                         </Accordion>
                     </CardBody>
                 </Card>
