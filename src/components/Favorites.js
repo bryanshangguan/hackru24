3d8356e987c099a642ab1baa97511ff1eadeb1dd
chrischan04 <cwhc2021@gmail.com>
2024-03-24 04:56:40 -0400

    commit

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Card, CardBody, Accordion, AccordionItem } from '@nextui-org/react';
 
-function Favorites() {
+function Favorites({favorites}) {
+    if (favorites == null || favorites.length === 0) {
+        return <p>No favorites added yet.</p>;
+    }
+
+    console.log(favorites);
+
     return (
-        <div className='history-page'>
-            <h1>History page</h1>
+        <div className="Favorites-page px-20 space-y-4 mt-4">
+            <h2 className="text-3xl font-bold mb-4">Your Favorite Recipes</h2>
+            {favorites.map((recipe, index) => (
+                <Card key={index}>
+                    <CardBody>
+                        <Accordion>
+                            <AccordionItem title={recipe.name}>
+                                {recipe.steps.map((step, stepIndex) => (
+                                    <p key={stepIndex}>{step}</p>
+                                ))}
+                            </AccordionItem>
+                        </Accordion>
+                    </CardBody>
+                </Card>
+            ))}
         </div>
     );
 }
