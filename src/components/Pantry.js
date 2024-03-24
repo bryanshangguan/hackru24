6bcdb8da988b4643e030be0065b58c00641bc98d
Amanda-Lei <amandaylei@gmail.com>
2024-03-24 04:55:37 -0400

    remove item functionality

--- src/components/Pantry.js
+++ src/components/Pantry.js
@@ -6,6 +6,7 @@ import proteinIcon from '../img/pantryIcons/proteinIcon.png';
 import dairyIcon from '../img/pantryIcons/dairyIcon.png';
 import carbsIcon from '../img/pantryIcons/carbsIcon.png';
 import seasonIcon from '../img/pantryIcons/seasonIcon.png';
+import removeIcon from '../img/pantryIcons/removeIcon.png';
 import { Modal, ModalContent, ModalHeader, ModalBody } from "@nextui-org/react";
 
 function Pantry() {
@@ -19,6 +20,7 @@ function Pantry() {
 
     const [modalOpen, setModalOpen] = useState(false);
     const [modalContent, setModalContent] = useState([]);
+    const [currentCategory, setCurrentCategory] = useState('');
 
     const addToCat = (category) => {
         switch (category) {
@@ -53,6 +55,7 @@ function Pantry() {
     };
 
     const openModalWithContent = (category) => {
+        setCurrentCategory(category);
         switch (category) {
             case 'fruit':
                 setModalContent(fruits);
@@ -78,6 +81,32 @@ function Pantry() {
         }
         setModalOpen(true);
     };
+
+    const handleRemoveItem = (item) => {
+        switch (currentCategory) {
+            case 'fruit':
+                setFruits(fruits.filter(fruit => fruit !== item));
+                break;
+            case 'veggie':
+                setVegetables(vegetables.filter(vegetable => vegetable !== item));
+                break;
+            case 'protein':
+                setProtein(protein.filter(proteinItem => proteinItem !== item));
+                break;
+            case 'dairy':
+                setDairy(dairy.filter(dairyItem => dairyItem !== item));
+                break;
+            case 'carbs':
+                setCarbs(carbs.filter(carb => carb !== item));
+                break;
+            case 'season':
+                setSeason(season.filter(seasonItem => seasonItem !== item));
+                break;
+            default:
+                break;
+        }
+        setModalOpen(false);
+    };    
 
     return (
         <div className='flex flex-col items-center h-screen my-3'>
@@ -116,8 +145,8 @@ function Pantry() {
                 <button className="bg-gray-200 rounded-full h-64 w-64 mt-12 my-2 relative" onClick={() => openModalWithContent('fruit')}>
                     <img src={fruitIcon} alt="fruit" className="object-cover rounded-full h-full w-full" />
                 </button>
-                <button className="bg-gray-200 rounded-full h-64 w-64 mt-12 my-2 relative" onClick={() => openModalWithContent('veggies')}>
-                    <img src={veggiesIcon} alt="veggies" className="object-cover rounded-full h-full w-full" />
+                <button className="bg-gray-200 rounded-full h-64 w-64 mt-12 my-2 relative" onClick={() => openModalWithContent('veggie')}>
+                    <img src={veggiesIcon} alt="veggie" className="object-cover rounded-full h-full w-full" />
                 </button>
                 <button className="bg-gray-200 rounded-full h-64 w-64 mt-12 my-2 relative" onClick={() => openModalWithContent('protein')}>
                     <img src={proteinIcon} alt="protein" className="object-cover rounded-full h-full w-full" />
@@ -138,17 +167,22 @@ function Pantry() {
             <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)}>
                 <ModalContent>
                     {(onClose) => (
-                        <>
-                            <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
-                            <ModalBody>
-                                <ul>
-                                    {modalContent.map((item, index) => (
-                                        <li key={index}>{item}</li>
-                                    ))}
-                                </ul>
-                                <button onClick={onClose}>Close Modal</button>
-                            </ModalBody>
-                        </>
+                    <>
+                        <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
+                        <ModalBody>
+                        <ul>
+                            {modalContent.map((item, index) => (
+                            <li key={index} className="flex justify-between items-center">
+                                <span>{item}</span>
+                                <button className="h-4 w-4" onClick={() => handleRemoveItem(item)}>
+                                <img src={removeIcon} alt="remove"/>
+                                </button>
+                            </li>
+                            ))}
+                        </ul>
+                        <button onClick={onClose}>Close Modal</button>
+                        </ModalBody>
+                    </>
                     )}
                 </ModalContent>
             </Modal>
